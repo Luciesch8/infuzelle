88a4dac2eefe23e1df12ae0fb1d8285d82af1b0f
Lucie <luciesch8@gmail.com>
2022-09-28 10:14:05 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { SharedModule } from './shared/shared.module';
+import { CalendrierComponent } from './shared/component/calendrier/calendrier.component';
+import { ReserverComponent } from './reserver/reserver.component';
+import { AdminModule } from './admin/admin.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path : "admin",
+  loadChildren: ()=> import('./admin/admin.module').then(m => AdminModule)
+  //component: CreateContactComponent
+},{
+  path : "",
+  component: AccueilComponent,
+},{
+  path : "menu",
+  component: MenuComponent,
+},{
+  path : "contact",
+  component: ContactComponent,
+},{
+  path : "actualite",
+  component: ActualiteComponent,
+},{
+  path : "reserver",
+  component : ReserverComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [
+  RouterModule,
+  SharedModule
+]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MenuComponent } from './menu/menu.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CarouselModule, SharedModule } from '@coreui/angular';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MenuDepliantComponent } from './shared/component/menu-depliant/menu-depliant.component';
+import { AdminComponent } from './admin/accueil/admin.component';
+import { ReserverComponent } from './reserver/reserver.component';
+import "@angular/compiler";
+import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    MenuComponent,
+    ActualiteComponent,
+    ContactComponent,
+    ReserverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
